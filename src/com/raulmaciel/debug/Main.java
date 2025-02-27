5fc5aead4480dcd575add36678b1b6ec971d4045
raulmaciel <raulmacielxd@gmail.com>
2025-02-27 10:25:07 -0300

    debug: testes da funções implementadas

--- src/com/raulmaciel/debug/Main.java
+++ src/com/raulmaciel/debug/Main.java
@@ -5,12 +5,15 @@ import com.raulmaciel.service.UsuarioService;
 import com.raulmaciel.util.FileUtil;
 import com.raulmaciel.util.UsuarioUtil;
 
+
 public class Main {
     public static void main(String[] args) {
         FileUtil.printForm(FileUtil.readForm("data/formulario.txt"));
         UsuarioService usuarioService = new UsuarioService();
         Usuario usuario = usuarioService.cadastrarUsuario();
+
         UsuarioUtil.imprimeUsuario(usuario);
+        UsuarioService.salvarUsuario(usuario);
 
     }
 }
