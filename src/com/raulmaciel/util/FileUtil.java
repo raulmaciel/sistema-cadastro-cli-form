65175644c7d511f3250bcc8d660d1fe066709067
raulmaciel <raulmacielxd@gmail.com>
2025-02-28 14:36:52 -0300

    feat: criado metodo de listar arquivos dentro de um diretorio

--- src/com/raulmaciel/util/FileUtil.java
+++ src/com/raulmaciel/util/FileUtil.java
@@ -5,6 +5,8 @@ import com.raulmaciel.exception.LeituraArquivoException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileUtil {
 
@@ -41,5 +43,39 @@ public class FileUtil {
             System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
         }
     }
+
+    public static void listarArquivos(String diretorioPath){
+        try {
+            File diretorio = new File(diretorioPath);
+            if (diretorio.exists() && diretorio.isDirectory()){
+                File[] arquivos = diretorio.listFiles();
+
+                if (arquivos != null){
+                for (File arquivo : arquivos) {
+                    try(BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+                        String nomeArquivo = arquivo.getName();
+                        String regex = "^\\d-[A-Z]+.txt";
+
+                        Pattern pattern = Pattern.compile(regex);
+                        Matcher matcher = pattern.matcher(nomeArquivo);
+
+                        if(matcher.find()){
+                            System.out.println(arquivo.getName().charAt(0) + " - "+ reader.readLine());
+                        }
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                }else {
+                    System.out.println("O diretorio não possui arquivos");
+                }
+
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
