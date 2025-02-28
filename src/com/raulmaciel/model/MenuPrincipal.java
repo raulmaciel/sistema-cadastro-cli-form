9eedfa55807308a43793ec20702fd010b72c37c3
raulmaciel <raulmacielxd@gmail.com>
2025-02-28 08:32:14 -0300

    feat: commit inicial desenvolvimento menu

---
+++ src/com/raulmaciel/model/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package com.raulmaciel.model;
+
+public class MenuPrincipal {
+    public static void callMenu(){
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("1- Cadastrar usuário");
+        System.out.println("2- Listar todos os usuários cadastrados");
+        System.out.println("3- Cadastrar nova pergunta no formulário");
+        System.out.println("4- Deletar uma pergunta do formulário");
+        System.out.println("5- Pesquisar usuário por nome ou idade ou email");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+}
